4a239609ff6d8d4917f27aa708c4c9acbfbbec3c
vghdse <h.hanayan66@gmail.com>
2025-08-13 23:21:52 +0200

    Update settings.js for Latran

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;TNEWSaCB#4-Pt6X6_2yBmHRN1EPfU8nfWJtJH08HFD1B-PptH70Y",
+  OWNER_NUMBER: "+237 6 93 73 38 69", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
