a809d9070b227147750b2be807443cf175b42620
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-14 18:58:01 +0200

    Update settings.js for Meeenu

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;LYMVATCQ#vlIutG6yqCITCPpgpm_N5Jy5sgMY5D6yBVD-5I-w6j4",
+  OWNER_NUMBER: "92341627813", 
+  PREFIX: ","
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
