5827bb75020b1e14f19503d20bee4de1a9259d25
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-15 09:02:10 +0200

    Update settings.js for Sabiiti

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;Gc80gI4D#5Wd97irAJgkVLSKIlN4vsMGR4Fm7Ido1R_m69NQA5Mw",
+  OWNER_NUMBER: "+256775995775", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
