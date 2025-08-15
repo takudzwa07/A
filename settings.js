93399091df227d7d09b0030085dd9a2a3f55ee8e
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-15 23:33:02 +0200

    Update settings.js for Buchi-B-wy-bot

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;rc1h2Q6a#qxex18eJEDUelMZbVIB5vtSnQV257b5G3ZTd-lEx72c",
+  OWNER_NUMBER: "+233502034065", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
