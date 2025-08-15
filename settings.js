72edbd3daad32d486fef51484d4d70c81964fe07
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-15 17:47:47 +0200

    Update settings.js for Aloma

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;eRE3gLpa#XV0rjccB3xtMRNOpyQvFVMsVMW9Eemf_d7q-E7p9DAA",
+  OWNER_NUMBER: "2347048625339", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
