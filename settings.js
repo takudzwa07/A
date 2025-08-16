807926006bd35e598d152a98ffb3e223f741132c
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-16 15:27:19 +0200

    Update settings.js for Lovemore

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;aMFHERIS#SZCQjRHJi4APzUOQkBtEWj1Lc9UGdh_1Pyc3uYF6LaA",
+  OWNER_NUMBER: "+263789846423", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
