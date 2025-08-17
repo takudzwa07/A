dd68518dab0977ab5acc6c248b79c96dbdd7fd9c
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-17 06:11:22 +0200

    Update settings.js for Matip

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;bcMShSIB#Lv-HTe0OPA_mZPci8kN0o8EbkcdjjzH2fQbTEeWyFv8",
+  OWNER_NUMBER: "2348135002936", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
