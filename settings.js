5df3acf6187b2ca174d49022a268c80392499f63
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-17 22:07:17 +0200

    Update settings.js for my-barrack-bot

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;2QN2Da4Q#ir6NMlKRcEWqCunmicXLM3QbvfnHYJz7fE3O3DOkgaA",
+  OWNER_NUMBER: "+256776423289", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
